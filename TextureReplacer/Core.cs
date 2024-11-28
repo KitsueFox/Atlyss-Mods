c33b85ebdf619cf6e50fd010c030005d094797b8
BlackyFox <blacky@blackyfox.net>
2024-11-28 13:37:46 -0600

    init commit

---
+++ TextureReplacer/Core.cs
@@ -0,0 +1,141 @@
+﻿using MelonLoader;
+using MelonLoader.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+namespace TextureReplacer
+{
+    public static class BuildInfo
+    {
+        public const string Name = "TextureReplacer";
+        public const string Author = "BlackyFox";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+    
+   public struct ReplacedTexture
+    {
+        public byte[] hash;
+        public Texture textures;
+        public HashSet<int> instances;
+    }
+    public class Texture_Replacer : MelonMod
+    {
+        //public static MelonPreferences_Category config;
+        //public static MelonPreferences_Entry<bool> reloadChangedTextures;
+
+        public static Dictionary<string, ReplacedTexture> loaded = new Dictionary<string, ReplacedTexture>();
+        public static bool reloadChangedTextures;
+        
+        private readonly string folderPath = Path.Combine(MelonEnvironment.UserDataDirectory, "CustomTextures\\");
+        private readonly string imageExtension = ".png";
+
+        public override void OnInitializeMelon()
+        {
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+            
+            var category = MelonPreferences.CreateCategory("TextureReplacer", "");
+            category.CreateEntry("reloadChangedTextures", false);
+            reloadChangedTextures = category.GetEntry<bool>("reloadChangedTextures").Value;
+            MelonPreferences.Save();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            LoggerInstance.Msg($"Loading texture replacements for scene \"{sceneName}\".");
+
+            HashSet<string> replacements = Directory.GetFiles(folderPath, "*" + imageExtension).Select(p => Path.GetFileNameWithoutExtension(p)).ToHashSet();
+            LoggerInstance.Msg($"Found {replacements.Count} *{imageExtension} texture replacements in \"{folderPath}\".");
+            
+            Texture2D[] textures = Resources.FindObjectsOfTypeAll<Texture2D>();
+            LoggerInstance.Msg($"Found {textures.Length} Texture2Ds.");
+            for (int i = 0; i < textures.Length; i++)
+            {
+                Texture2D texture = textures[i];
+                string texName = texture.name;
+                if (replacements.Contains(texName))
+                {
+                    string filePath = folderPath + texName + imageExtension;
+                    if (!loaded.TryGetValue(texName, out ReplacedTexture replacedTexture))
+                    {
+                        LoggerInstance.Msg($"Replacement texture for \"{texName}\" has not yet been loaded, reading from file...");
+                        byte[] data = File.ReadAllBytes(filePath);
+
+                        if (!texture.LoadImage(data, true))
+                        {
+                            LoggerInstance.Msg($"Failed to load texture \"{texName}\".");
+                        }
+                        else
+                        {
+                            byte[] hash = { 0 };
+                            if (reloadChangedTextures)
+                            {
+                                using (MD5 md5 = MD5.Create())
+                                {
+                                    hash = md5.ComputeHash(data);
+                                }
+                                LoggerInstance.Msg($"Calculated hash for \"{texName}\": {HashToString(hash)}");
+                            }
+
+                            HashSet<int> instances = new HashSet<int>
+                            {
+                                texture.GetInstanceID()
+                            };
+                            loaded[texName] = new ReplacedTexture { hash = hash, textures = texture, instances = instances };
+                        }
+                    }
+                    else
+                    {
+                        foreach (var item in replacedTexture.instances)
+                        {
+                            LoggerInstance.Msg(item);
+                        }
+
+                        if (replacedTexture.instances.Add(texture.GetInstanceID()))
+                        {
+                            LoggerInstance.Msg($"Copying already replaced texture \"{texName}\", as a new Texture2D with the same name but a different ID was found...");
+                            Graphics.CopyTexture(replacedTexture.textures, texture);
+                        }
+                        else if (reloadChangedTextures)
+                        {
+                            byte[] data = File.ReadAllBytes(filePath);
+                            using (MD5 md5 = MD5.Create())
+                            {
+                                byte[] newHash = md5.ComputeHash(data);
+                                ReplacedTexture loadedTexture = loaded[texName];
+                                byte[] oldHash = loadedTexture.hash;
+                                if (!newHash.SequenceEqual(oldHash))
+                                {
+                                    LoggerInstance.Msg($"Hash for \"{texName}\" changed! Old hash {HashToString(oldHash)}, new hash: {HashToString(newHash)}. Loading new texture...");
+                                    textures[i].LoadImage(data);
+                                    loadedTexture.hash = newHash;
+                                    loaded[texName] = loadedTexture;
+                                }
+                                else
+                                {
+                                    LoggerInstance.Msg($"Hash for \"{texName}\" is unchanged. Skipping...");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            LoggerInstance.Msg($"Replacement texture for \"{texName}\" already loaded and reloading is disabled, skipping...");
+                        }
+                    }
+                }
+            }
+        }
+        
+        private string HashToString(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", String.Empty);
+        }
+    }
+}
---
+++ TextureReplacer/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using TextureReplacer;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(TextureReplacer.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(TextureReplacer.BuildInfo.Company)]
+[assembly: AssemblyProduct(TextureReplacer.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created By " + TextureReplacer.BuildInfo.Author)]
+[assembly: AssemblyTrademark(TextureReplacer.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("FD3630D8-19E6-4819-9B3B-FB27B336FA50")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(TextureReplacer.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(TextureReplacer.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Melonloader
+[assembly: MelonInfo(typeof(Texture_Replacer), "Texture Replacement", "0.0.1", "BlackyFox")]
+[assembly: MelonGame("KisSoft", "ATLYSS")]
