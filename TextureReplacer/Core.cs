7bc8efe95524e91b37fbe5c584a50f49dda6e268
BlackyFox <blacky@blackyfox.net>
2024-11-28 13:37:56 -0600

    init commit

--- TextureReplacer/Core.cs
+++ TextureReplacer/Core.cs
@@ -15,7 +15,7 @@ namespace TextureReplacer
         public const string Name = "TextureReplacer";
         public const string Author = "BlackyFox";
         public const string Company = null;
-        public const string Version = "1.0.0";
+        public const string Version = "0.1.0";
         public const string DownloadLink = null;
     }
     
